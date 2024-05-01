6fa6cc4af5781062d92993e21a970dc62555536c
Guenther Sohler <guenther.sohler@photeon.com>
2024-05-01 20:43:29 +0200

    rotate_extrude also understands "v" parameter

--- src/geometry/rotextrude.cc
+++ src/geometry/rotextrude.cc
@@ -31,21 +31,21 @@ void  append_rotary_vertex(PolySetBuilder &builder,const Outline2d *face, int in
 
 
 
-static void fill_ring(std::vector<Vector3d>& ring, const Outline2d& o, double a, bool flip)
+static void fill_ring(std::vector<Vector3d>& ring, const std::vector<Vector2d> & vertices, double a, Vector3d dv, double fact, double xmid,bool flip)
 {
-  if (flip) {
-    unsigned int l = o.vertices.size() - 1;
-    for (unsigned int i = 0; i < o.vertices.size(); ++i) {
-      ring[i][0] = o.vertices[l - i][0] * sin_degrees(a);
-      ring[i][1] = o.vertices[l - i][0] * cos_degrees(a);
-      ring[i][2] = o.vertices[l - i][1];
-    }
-  } else {
-    for (unsigned int i = 0; i < o.vertices.size(); ++i) {
-      ring[i][0] = o.vertices[i][0] * sin_degrees(a);
-      ring[i][1] = o.vertices[i][0] * cos_degrees(a);
-      ring[i][2] = o.vertices[i][1];
-    }
+  unsigned int l = vertices.size() - 1;
+  for (unsigned int i = 0; i < vertices.size(); ++i) {
+    unsigned int j = flip?l - i : i;	  
+    //
+    // cos(atan(x))=1/sqrt(1+x*x)
+    // sin(atan(x))=x/sqrt(1+x*x)
+    double tan_pitch= fact/(std::isnan(xmid)?vertices[j][0]:xmid);
+    double cf=1/sqrt(1+tan_pitch*tan_pitch);
+    double sf=cf*tan_pitch;
+    Vector3d centripedal=Vector3d( cos_degrees(a), sin_degrees(a) ,0);
+    Vector3d progress=Vector3d(-sin_degrees(a)*cf, cos_degrees(a)*cf, sf);
+    Vector3d upwards=centripedal.cross(progress);
+    ring[i] =  centripedal * vertices[j][0] + upwards * vertices[j][1] + dv;
   }
 }
 
@@ -67,31 +67,16 @@ static void fill_ring(std::vector<Vector3d>& ring, const Outline2d& o, double a,
    Currently, we generate a lot of zero-area triangles
 
  */
-std::shared_ptr<Geometry> rotatePolygon(const RotateExtrudeNode& node, const Polygon2d& poly)
+std::unique_ptr<Geometry> rotatePolygonSub(const RotateExtrudeNode& node, const Polygon2d& poly, int fragments, int fragstart, int fragend, bool flip_faces)
 {
-  if (node.angle == 0) return nullptr;
 
   PolySetBuilder builder;
   builder.setConvexity(node.convexity);
 
-  double min_x = 0;
-  double max_x = 0;
-  unsigned int fragments = 0;
-  for (const auto& o : poly.outlines()) {
-    for (const auto& v : o.vertices) {
-      min_x = fmin(min_x, v[0]);
-      max_x = fmax(max_x, v[0]);
-    }
-  }
 
-  if ((max_x - min_x) > max_x && (max_x - min_x) > fabs(min_x)) {
-    LOG(message_group::Error, "all points for rotate_extrude() must have the same X coordinate sign (range is %1$.2f -> %2$.2f)", min_x, max_x);
-    return nullptr;
-  }
 
-  fragments = (unsigned int)std::ceil(fmax(Calc::get_fragments_from_r(max_x - min_x, 360.0, node.fn, node.fs, node.fa) * std::abs(node.angle) / 360, 1));
+  double fact=(node.v[2]/node.angle)*(180.0/G_PI);
 
-  bool flip_faces = (min_x >= 0 && node.angle > 0 && node.angle != 360) || (min_x < 0 && (node.angle < 0 || node.angle == 360));
 
 #ifdef ENABLE_PYTHON  
   if(node.profile_func != NULL)
@@ -102,32 +87,40 @@ std::shared_ptr<Geometry> rotatePolygon(const RotateExtrudeNode& node, const Pol
 	double last_ang=0, cur_ang=0;
 	double last_twist=0.0, cur_twist=0.0;
 
-#ifdef ENABLE_PYTHON
         if(node.twist_func != NULL) {
           last_twist = python_doublefunc(node.twist_func, 0);
         } else last_twist=0;
-#endif	
 	
 
-		lastFace = alterprofile(python_getprofile(node.profile_func, node.fn, 0),1.0, 1.0,node.origin_x, node.origin_y, node.offset_x, node.offset_y, last_twist);
-	if(node.angle != 360) {
-		// Add initial closing
-		Polygon2d lastface;
-	        lastface.addOutline(lastFace);
-	        std::unique_ptr<PolySet> ps_last = lastface.tessellate();
+	lastFace = alterprofile(python_getprofile(node.profile_func, node.fn, 0),1.0, 1.0,node.origin_x, node.origin_y, node.offset_x, node.offset_y, last_twist);
+	if(node.angle != 360 || node.v.norm() > 0) {
+    	  auto ps = poly.tessellate(); // starting face
+          double xmid=NAN;
+          if(node.method == "centered") {
+            double xmin, xmax;
+            xmin=xmax=ps->vertices[0][0];
+            for(const auto &v : ps->vertices) {
+              if(v[0] < xmin) xmin=v[0];	    
+              if(v[0] > xmax) xmax=v[0];	    
+            }
+            xmid=(xmin+xmax)/2;
+          } 
 
-		Transform3d rot(angle_axis_degrees(90, Vector3d::UnitX()));
-		ps_last->transform(rot);
-		// Flip vertex ordering
-		if (!flip_faces) {
-		for (auto& p : ps_last->indices) {
-		std::reverse(p.begin(), p.end());
-		}
-		}
-		builder.appendPolySet(*ps_last);
+          std::vector<Vector3d> ring;
+          ring.resize(3);
+          for (auto& p : ps->indices) {
+            std::vector<Vector2d> vertices;
+            for(int j=0;j<3;j++)
+            vertices.push_back(ps->vertices[p[j]].head<2>());
 
+            fill_ring(ring , vertices, node.angle*fragstart/fragments, node.v*fragstart/fragments, fact, xmid, !flip_faces); // close start
+            builder.appendPolygon(ring);
+
+            fill_ring(ring, vertices, node.angle*fragend/fragments   , node.v*fragend/fragments  , fact, xmid, flip_faces); // close end
+            builder.appendPolygon(ring);
+          }
 	}
-  	for (unsigned int i = 1; i <= fragments; i++) {
+  	for (unsigned int i = fragstart + 1; i <= fragend; i++) {
 		cur_ang=i*node.angle/fragments;
 
 		if(node.twist_func != NULL) {
@@ -155,45 +148,36 @@ std::shared_ptr<Geometry> rotatePolygon(const RotateExtrudeNode& node, const Pol
 		last_ang = cur_ang;
 		last_twist = cur_twist;
 	}
-	if(node.angle != 360) {
-		Polygon2d curface;
-	        curface.addOutline(curFace);
-		std::unique_ptr<PolySet> ps_cur = curface.tessellate();
-		Transform3d rot2(angle_axis_degrees(cur_ang, Vector3d::UnitZ()) * angle_axis_degrees(90, Vector3d::UnitX()));
-		ps_cur->transform(rot2);
-		if (flip_faces) {
-			for (auto& p : ps_cur->indices) {
-				std::reverse(p.begin(), p.end());
-			}
-		}
-		builder.appendPolySet(*ps_cur);
-	}
-	  
-}
+       }
   else
 #endif
   {	  
-  if (node.angle != 360) {
-    auto ps_start = poly.tessellate(); // starting face
-    Transform3d rot(angle_axis_degrees(90, Vector3d::UnitX()));
-    ps_start->transform(rot);
-    // Flip vertex ordering
-    if (!flip_faces) {
-      for (auto& p : ps_start->indices) {
-        std::reverse(p.begin(), p.end());
+  if (node.angle != 360 || node.v.norm() > 0) {
+    auto ps = poly.tessellate(); // starting face
+    double xmid=NAN;
+    if(node.method == "centered") {
+      double xmin, xmax;
+      xmin=xmax=ps->vertices[0][0];
+      for(const auto &v : ps->vertices) {
+        if(v[0] < xmin) xmin=v[0];	    
+        if(v[0] > xmax) xmax=v[0];	    
       }
-    }
-    builder.appendPolySet(*ps_start);
+      xmid=(xmin+xmax)/2;
+    }  
 
-    auto ps_end = poly.tessellate();
-    Transform3d rot2(angle_axis_degrees(node.angle, Vector3d::UnitZ()) * angle_axis_degrees(90, Vector3d::UnitX()));
-    ps_end->transform(rot2);
-    if (flip_faces) {
-      for (auto& p : ps_end->indices) {
-        std::reverse(p.begin(), p.end());
-      }
+    std::vector<Vector3d> ring;
+    ring.resize(3);
+    for (auto& p : ps->indices) {
+      std::vector<Vector2d> vertices;
+      for(int j=0;j<3;j++)
+        vertices.push_back(ps->vertices[p[j]].head<2>());
+
+      fill_ring(ring , vertices, node.angle*fragstart/fragments, node.v*fragstart/fragments, fact, xmid, !flip_faces); // close start
+      builder.appendPolygon(ring);
+
+      fill_ring(ring, vertices, node.angle*fragend/fragments   , node.v*fragend/fragments  , fact, xmid, flip_faces); // close end
+      builder.appendPolygon(ring);
     }
-    builder.appendPolySet(*ps_end);
   }
 
   for (const auto& o : poly.outlines()) {
@@ -201,13 +185,29 @@ std::shared_ptr<Geometry> rotatePolygon(const RotateExtrudeNode& node, const Pol
     rings[0].resize(o.vertices.size());
     rings[1].resize(o.vertices.size());
 
-    fill_ring(rings[0], o, (node.angle == 360) ? -90 : 90, flip_faces); // first ring
-    for (unsigned int j = 0; j < fragments; ++j) {
-      double a;
-      if (node.angle == 360) a = -90 + ((j + 1) % fragments) * 360.0 / fragments; // start on the -X axis, for legacy support
-      else a = 90 - (j + 1) * node.angle / fragments; // start on the X axis
-      fill_ring(rings[(j + 1) % 2], o, a, flip_faces);
+    double xmid=NAN;
 
+    if(node.method == "centered") {
+      double xmin, xmax;
+      xmin=xmax=o.vertices[0][0];
+      for(const auto &v : o.vertices) {
+        if(v[0] < xmin) xmin=v[0];	    
+        if(v[0] > xmax) xmax=v[0];	    
+      }
+      xmid=(xmin+xmax)/2;
+    }  
+    Vector3d dv = node.v*fragstart/fragments;
+    double a;
+    if (node.angle == 360 && node.v.norm() == 0) a=180;
+     else a = fragstart * node.angle / fragments;
+
+    fill_ring(rings[fragstart % 2 ], o.vertices, a, dv,fact,  xmid, flip_faces); // first ring
+
+    for (unsigned int j = fragstart; j < fragend; ++j) {
+      dv = node.v*(j+1)/fragments;
+      if (node.angle == 360 && node.v.norm() == 0) a = 180 - ((j + 1) % fragments) * 360.0 / fragments; // start on the -X axis, for legacy support
+      else a = (j + 1) * node.angle / fragments; // start on the X axis
+      fill_ring(rings[(j + 1) % 2], o.vertices, a, dv, fact, xmid, flip_faces);
       for (size_t i = 0; i < o.vertices.size(); ++i) {
         builder.appendPolygon({
                 rings[j % 2][(i + 1) % o.vertices.size()],
@@ -226,5 +226,60 @@ std::shared_ptr<Geometry> rotatePolygon(const RotateExtrudeNode& node, const Pol
   }
   return builder.build();
 }
+
+std::shared_ptr<Geometry> rotatePolygon(const RotateExtrudeNode& node, const Polygon2d& poly){
+
+  if (node.angle == 0) return nullptr;
+  double min_x = 0;
+  double max_x = 0;
+  double min_y = 0;
+  double max_y = 0;
+  unsigned int fragments = 0;
+  for (const auto& o : poly.outlines()) {
+    for (const auto& v : o.vertices) {
+      min_x = fmin(min_x, v[0]);
+      max_x = fmax(max_x, v[0]);
+      min_y = fmin(min_y, v[1]);
+      max_y = fmax(max_y, v[1]);
+    }
+  }
+
+  if ((max_x - min_x) > max_x && (max_x - min_x) > fabs(min_x)) {
+    LOG(message_group::Error, "all points for rotate_extrude() must have the same X coordinate sign (range is %1$.2f -> %2$.2f)", min_x, max_x);
+    return nullptr;
+  }
+  fragments = (unsigned int)std::ceil(fmax(Calc::get_fragments_from_r(max_x - min_x, node.angle, node.fn, node.fs, node.fa) * std::abs(node.angle) / 360, 1));
+  bool flip_faces = (min_x >= 0 && node.angle > 0 && node.angle != 360) || (min_x < 0 && (node.angle < 0 || node.angle == 360));
+
+  // check if its save to extrude
+  bool safe=true;
+  do
+  {
+    if(node.angle < 300) break;	  
+    if(node.v.norm() == 0) break;
+    if(node.v[2]/(node.angle/360.0)  >  (max_y-min_y) * 1.5) break;
+    safe=false;
+
+  } while(false);
+  if(safe) return rotatePolygonSub(node, poly, fragments, 0, fragments, flip_faces);	
+
+  // now create a fragment splitting plan
+  int splits=ceil(node.angle/300.0);
+  int fragstart=0,fragend;
+  std::shared_ptr<ManifoldGeometry> result = nullptr;
+
+  for(int i=0;i<splits;i++) {
+    fragend=fragstart+(fragments/splits)+1;    	 
+    if(fragend > fragments) fragend=fragments;
+    std::unique_ptr<Geometry> part_u =rotatePolygonSub(node, poly, fragments, fragstart, fragend, flip_faces);	
+    std::shared_ptr<Geometry> part_s = std::move(part_u);
+    std::shared_ptr<const ManifoldGeometry>term = ManifoldUtils::createManifoldFromGeometry(part_s);
+    if(result == nullptr) result = std::make_shared<ManifoldGeometry>(*term);
+      else *result = *result + *term;	
+    fragstart=fragend-1;
+  }
+  return  result; 
+}
+
 
 
