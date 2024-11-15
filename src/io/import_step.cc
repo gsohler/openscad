a6a4fa1418c984917d0a7b05d9061f1efc128f93
Guenther Sohler <guenther.sohler@photeon.com>
2024-11-16 00:03:06 +0100

    can extract coordinates from step cube

--- src/io/import_step.cc
+++ src/io/import_step.cc
@@ -5,57 +5,47 @@
 // https://stepcode.github.io/docs/p21_cpp_example/
 //#include <sc_cf.h>
 extern void SchemaInit( class Registry & );
-#include "STEPfile.h"
-#include "sdai.h"
-#include <STEPattribute.h>
-#include <ExpDict.h>
-#include <Registry.h>
-#include <errordesc.h>
+#include "StepKernel.h"
 
-//#include <ios>
-//#include <memory>
-//#include <fstream>
-//#include <string>
-//#include <vector>
-//#include <boost/regex.hpp>
-//#include <boost/lexical_cast.hpp>
-//#include <boost/algorithm/string.hpp>
-//#include <boost/algorithm/string/split.hpp>
-
+// https://github.com/slugdev/stltostp/blob/master/StepKernel.cpp
 std::unique_ptr<PolySet> import_step(const std::string& filename, const Location& loc) {
   PolySetBuilder builder;
   printf("Importing %s\n",filename.c_str());
+  StepKernel sk;
+  sk.read_step(filename);
+  for(auto  *e : sk.entities) {
+    const StepKernel::ManifoldSolid *mani = dynamic_cast<StepKernel::ManifoldSolid *>(e);
+    if (mani == nullptr) continue;
+    printf("Found Manifold\n");
+    StepKernel::Shell *shell = mani->shell;
+    printf("shell node is %p\n",shell);
+    if(shell == nullptr) continue;
+    printf("t %d\n",shell->faces.size());		   
+    for(int i=0;i<shell->faces.size();i++) {
+      printf("Face %d\n",i);
+      StepKernel::Face *face = shell->faces[i];
+      if(face == nullptr) continue;
+      printf("%d bounds\n",face->faceBounds.size());
+      for(int j=0;j<face->faceBounds.size();j++) {
+        printf("Bound %d\n",j);	     
+        StepKernel::FaceBound *bound = face->faceBounds[j];
+	if(bound == nullptr) continue;
+	StepKernel::EdgeLoop *loop = bound->edgeLoop;
+	for(int k=0;k<loop->faces.size();k++) {
+	  StepKernel::OrientedEdge *edge=loop->faces[k];
+	  if(edge == nullptr) continue;
+	  StepKernel::EdgeCurve *edgecurv = edge->edge;
+	  StepKernel::Point *pt1 = edgecurv->vert1->point;
+	  StepKernel::Point *pt2 = edgecurv->vert2->point;
+	  printf("(%g/%g/%g) - (%g/%g/%g)\n",pt1->x, pt1->y, pt1->z, pt2->x, pt2->y, pt2->z);
+	}
+       	
+      }
+    }
+  }  
+  // https://github.com/FreeCAD/FreeCAD/blob/main/src/Mod/Part/App/ImportStep.cpp
+/*
 
-  // the registry contains information about types present in the current schema; SchemaInit 
-    // is a function in the schema-specific SDAI library
-  Registry  registry( SchemaInit );
-
-  //the InstMgr holds instances that have been created or that have been loaded from a file
-  InstMgr   instance_list;
-
-  // STEPfile takes care of reading and writing Part 21 files
-  STEPfile  sfile( registry, instance_list, "", false );
-
-  // read a file, using the name from the command line
-  sfile.ReadExchangeFile(filename.c_str());
-
-  // check for errors; exit if they are particularly severe
-  if( sfile.Error().severity() < SEVERITY_USERMSG ) {
-        sfile.Error().PrintContents( cout );
-  }
-  if ( sfile.Error().severity() <= SEVERITY_INCOMPLETE ) {
-        exit(1);
-  }
-
-  /**************************************************
-  ** do something with the data here
-  ***************************************************/
-
-  // write to "file.out", then check for write errors. The write operation overwrites any
-  // errors caused by previous operations.
-//  sfile.WriteExchangeFile( "file.out" );
-//  if( sfile.Error().severity() < SEVERITY_USERMSG ) {
-//    sfile.Error().PrintContents( cout );
-//  }
+*/
   return builder.build();
-}
+} 
